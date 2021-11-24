68869a04bef04ff7e9e7be06ec7607bbcaf60958
Philipp Oeschger <philipp.oeschger@hs-furtwangen.de>
2021-11-24 15:08:23 +0100

    kommentare Musterlösung Überblick

--- Überblick/script_musterLoesung.ts
+++ Überblick/script_musterLoesung.ts
@@ -61,24 +61,24 @@ for (const entry of events) {
 
 // c)
 function maxPrice(array: any[][]): number {
-  let maxPrice: number = 0;
-  for (const entry of array) {
+  let maxPrice: number = 0; //Maximalpreis mit 0 initialisieren
+  for (const entry of array) { //Iteration
     if (maxPrice < entry[1]) {
-      maxPrice = entry[1];
+      maxPrice = entry[1]; // Wenn der aktuell Wert größer als der aktuelle Maximapreis ist, überschreibe den Aktuelle Maximalwert
     }
   }
-  return maxPrice;
+  return maxPrice; //Gibt am Ende der Iteration den Maximalpreis zurück
 }
 console.log("Max Price:", maxPrice(events)); // Max Price: 25.2
 
 // d)
 function hasInterpret(array: any[][], interpret: string): boolean {
-  for (const entry of array) {
-    if (entry[0].toLowerCase() === interpret.toLowerCase()) {
-      return true;
+  for (const entry of array) { //Iteration
+    if (entry[0].toLowerCase() === interpret.toLowerCase()) { //mache beide Strings zu Kleinbuchstaben
+      return true; //Wenn beide Strings gleich sind, wurde der Interpret gefunden und es kann "true" zurückgegeben werden
     }
   }
-  return false;
+  return false; //Wenn die Interation bis zum Ende durchgelaufen ist, wurde Der Interpret nicht gefunden und es kann "false" zurüchgegeben werden
 }
 
 console.log("Metallica", hasInterpret(events, "Metallica")); // Ausgabe: "Metallica" true
@@ -87,13 +87,13 @@ console.log("mETALLICA", hasInterpret(events, "mETALLICA")); // Ausgabe: "mETALL
 
 // e)
 function factorial(n: number): void {
-  let idx: number = 1;
-  let result: number = 1;
-  while (idx <= n) {
-    result *= idx;
-    idx++;
+  let idx: number = 1; // lege eine Index an und setze ihn auf 1
+  let result: number = 1; //lege die Rückgabevariable an
+  while (idx <= n) { //solane der Index kleiner/ gleich n ist
+    result *= idx; // mulipliziere den Index mit dem aktuelle Resultat
+    idx++; //zähle den Index hoch
   }
-  console.log(`${n}!: ${result}`);
+  console.log(`${n}!: ${result}`); //Ausgabe der Fakultät
 }
 
 factorial(4); // 4!: 24
@@ -101,37 +101,41 @@ factorial(5); // 5!: 120
 factorial(10); // 10!: 3628800
 
 // f)
-let count: number = 0;
-for (let i: number = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    console.log(i);
-    count++;
+let count: number = 0; //Zähler mit 0 initialisieren
+for (let i: number = 1; i <= 100; i++) { // Durch alle Zahlen von 1 bis 100 durchiterieren
+  if (i % 3 === 0) { //Wenn die Zahl durch 3 teilbar ist und kein Rest (Modulo) überbleibt
+    console.log(i); // Ausgabe des Aktuelle Wertes
+    count++; // Zähler hochzählen
   }
 }
 // Ergebnis: 33
 
-console.log("Numbers devidable by 3:", count);
+console.log("Numbers devidable by 3:", count); // Zähler Ausgabe
 
 // g)
 class ConcertEvent {
+  // Attribute anlegen
   private interpret: string;
   private price: number;
 
+  //Konstruktor
   constructor(interpret: string, price: number) {
-    this.interpret = interpret;
+    // Werte mithilfe der Parameter initialisieren
+    this.interpret = interpret; 
     this.price = price;
   }
 
+  //Funktion für die Ausgabe
   show(): void {
     console.log(`Interpret: ${this.interpret}, Price: ${this.price}`);
   }
 }
 
 // h)
-let concertEventsArray: ConcertEvent[] = [];
+let concertEventsArray: ConcertEvent[] = []; //Array initialisieren
 
-for (const event of events) {
-  concertEventsArray.push(new ConcertEvent(event[0], event[1]));
+for (const event of events) { //durch altes Array durchiterieren
+  concertEventsArray.push(new ConcertEvent(event[0], event[1])); //mit den Werten Objekte vom Typ "ConcertEvent" instanziieren
 }
 
 for (const concertEvent of concertEventsArray) {
